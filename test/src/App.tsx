c04bb29646c162685f50d98ad252e288044dc99f
Ludea <ludovicw35@hotmail.com>
2023-07-18 14:10:48 +0000

    test project

---
+++ test/src/App.tsx
@@ -0,0 +1,13 @@
+import BLPImage from 'BLPImage';
+
+function App() {
+  const file = "./loadscreeneasternkingdomwide.blp";
+  new BLPImage(file)
+  return (
+  <div>
+    
+  </div>  
+  );
+}
+
+export default App
